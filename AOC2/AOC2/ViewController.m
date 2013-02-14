d8fd4da30d8fc6a44f222d23c488d225a3ed1999
Nick Stelzer <iamnick@fullsail.edu>
2013-02-14 17:00:37 -0500

    Title/Author/Publisher labels added

--- AOC2/AOC2/ViewController.m
+++ AOC2/AOC2/ViewController.m
@@ -23,13 +23,46 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-	UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 200.0f, 20.0f)];
-    if (label != nil) {
-    	label.text = @"this is a UILabel";
-        label.textAlignment = UITextAlignmentCenter;
+	// Title Label
+	UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 300.0f, 20.0f)];
+    if (titleLabel != nil) {
+    	titleLabel.text = @"The Outsiders";
+        titleLabel.textAlignment = UITextAlignmentCenter;
     }
-    [self.view addSubview:label];
-
+    
+    // Author Labels
+    UILabel *authorLabelLeft = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 40.0f, 145.0f, 20.0f)];
+    if (authorLabelLeft != nil) {
+    	authorLabelLeft.text = @"Author:";
+        authorLabelLeft.textAlignment = UITextAlignmentRight;
+    }
+    UILabel *authorLabelRight = [[UILabel alloc] initWithFrame:CGRectMake(165.0f, 40.0f, 145.0f, 20.0f)];
+    if (authorLabelRight != nil) {
+    	authorLabelRight.text = @"S.E. Hinton";
+        authorLabelRight.textAlignment = UITextAlignmentLeft;
+    }
+    
+    // Publisher Labels
+    UILabel *pubLabelLeft = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 70.0f, 145.0f, 20.0f)];
+    if (pubLabelLeft != nil) {
+    	pubLabelLeft.text = @"Published:";
+        pubLabelLeft.textAlignment = NSTextAlignmentRight;
+    }
+    UILabel *pubLabelRight = [[UILabel alloc] initWithFrame:CGRectMake(165.0f, 70.0f, 145.0f, 20.0f)];
+    if (pubLabelRight != nil) {
+    	pubLabelRight.text = @"1967";
+        pubLabelRight.textAlignment = NSTextAlignmentLeft;
+    }
+    
+    // Summary Labels
+    
+    // Add Labels to the View
+    [self.view addSubview:titleLabel];
+	[self.view addSubview:authorLabelLeft];
+    [self.view addSubview:authorLabelRight];
+    [self.view addSubview:pubLabelLeft];
+    [self.view addSubview:pubLabelRight];
+    
 	[super viewWillAppear:animated];
 }
 
