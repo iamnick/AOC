2289e71b364c375bd7c6ec89922912239693dad5
Nick Stelzer <iamnick@fullsail.edu>
2013-02-28 07:57:52 -0500

    Added Date and Info Elements, Defined Tags

--- AOC4/AOC4/ViewController.m
+++ AOC4/AOC4/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+#define LOGIN_BUTTON 0
+#define DATE_BUTTON 1
+#define INFO_BUTTON 2
+
 @interface ViewController ()
 
 @end
@@ -16,8 +20,6 @@
 
 - (void)viewDidLoad
 {
-	// Login Area
-    
     // Username Label
     UILabel *usernameLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 15.0f, 90.0f, 20.0f)];
     if (usernameLabel != nil) {
@@ -26,18 +28,20 @@
         [self.view addSubview:usernameLabel];
     }
     
-    // Login Text Field
+    // Username Text Field
     UITextField *usernameTextField = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
     if (usernameTextField != nil) {
     	usernameTextField.borderStyle = UITextBorderStyleRoundedRect;
         [self.view addSubview:usernameTextField];
     }
     
-	// Login Button
+	// Login Button (0)
 	UIButton *loginButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     if (loginButton != nil) {
     	loginButton.frame = CGRectMake(230.0f, 45.0f, 80.0f, 30.0f);
         [loginButton setTitle:@"Login" forState:UIControlStateNormal];
+        loginButton.tag = LOGIN_BUTTON;
+    	[loginButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
     	[self.view addSubview:loginButton];
     }
     
@@ -48,9 +52,42 @@
         loginMsgLabel.textAlignment = NSTextAlignmentCenter;
         [self.view addSubview:loginMsgLabel];
     }
+    
+    // Show Date Button (1)
+    UIButton *dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (dateButton != nil) {
+    	dateButton.frame = CGRectMake(110.0f, 200.0f, 100.0f, 40.0f);
+        dateButton.tag = DATE_BUTTON;
+        [dateButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [dateButton setTitle:@"Show Date" forState:UIControlStateNormal];
+    	[self.view addSubview:dateButton];
+    }
+    
+    // Info Button (2)
+    UIButton *infoButton = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    if (infoButton != nil) {
+    	infoButton.frame = CGRectMake(150.0f, 300.0f, 20.0f, 20.0f);
+        infoButton.tag = INFO_BUTTON;
+        [infoButton addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:infoButton];
+    }
+    
+    // Info Label
+    UILabel *infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 330.0f, 320.0f, 60.0f)];
+    if (infoLabel != nil) {
+    	[self.view addSubview:infoLabel];
+    }
 
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)onClick:(UIButton*)button
+{
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Popup" message:@"onClick function" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    if (alertView != nil) {
+    	[alertView show];
+    }
 }
 
 - (void)didReceiveMemoryWarning
