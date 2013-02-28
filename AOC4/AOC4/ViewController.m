40f9a37bd558915df790eb2818e44deae89c9335
Nick Stelzer <iamnick@fullsail.edu>
2013-02-28 13:04:29 -0500

    Date Section Done

--- AOC4/AOC4/ViewController.m
+++ AOC4/AOC4/ViewController.m
@@ -29,13 +29,13 @@
     }
     
     // Username Text Field
-    UITextField *usernameTextField = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
+    usernameTextField = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
     if (usernameTextField != nil) {
     	usernameTextField.borderStyle = UITextBorderStyleRoundedRect;
         [self.view addSubview:usernameTextField];
     }
     
-	// Login Button (0)
+	// Login Button
 	UIButton *loginButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     if (loginButton != nil) {
     	loginButton.frame = CGRectMake(230.0f, 45.0f, 80.0f, 30.0f);
@@ -46,14 +46,14 @@
     }
     
     // Login Message Label
-    UILabel *loginMsgLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 85.0f, 320.0f, 60.0f)];
+    loginMsgLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 85.0f, 320.0f, 60.0f)];
     if (loginMsgLabel != nil) {
     	loginMsgLabel.text = @"Please Enter Username";
         loginMsgLabel.textAlignment = NSTextAlignmentCenter;
         [self.view addSubview:loginMsgLabel];
     }
     
-    // Show Date Button (1)
+    // Show Date Button
     UIButton *dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     if (dateButton != nil) {
     	dateButton.frame = CGRectMake(110.0f, 200.0f, 100.0f, 40.0f);
@@ -63,7 +63,7 @@
     	[self.view addSubview:dateButton];
     }
     
-    // Info Button (2)
+    // Info Button 
     UIButton *infoButton = [UIButton buttonWithType:UIButtonTypeInfoLight];
     if (infoButton != nil) {
     	infoButton.frame = CGRectMake(150.0f, 300.0f, 20.0f, 20.0f);
@@ -84,10 +84,33 @@
 
 - (void)onClick:(UIButton*)button
 {
-	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Popup" message:@"onClick function" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	if (button.tag == LOGIN_BUTTON) {
+    	NSString *username = [usernameTextField text];
+        if (username.length == 0) {
+        	loginMsgLabel.text = @"Username cannot be empty";
+        } else {
+        	NSString *loginMsg = [[NSString alloc] initWithFormat:@"User: %@ has been logged in", username];
+        	loginMsgLabel.text = loginMsg;
+        }
+    } else if (button.tag = DATE_BUTTON) {
+    	NSDate *currentDate = [NSDate date];
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        if (dateFormatter != nil) {
+        	[dateFormatter setDateFormat:@"MMMM dd, y - hh:mm:ss a zzzz"];
+        }
+        
+        // Date Alert
+        UIAlertView *dateAlert = [[UIAlertView alloc] initWithTitle:@"Date" message:[dateFormatter stringFromDate:currentDate] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        if (dateAlert != nil) {
+        	[dateAlert show];
+        }
+    } else if (button.tag = INFO_BUTTON) {
+    	
+    }
+	/*UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Popup" message:@"onClick function" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
     if (alertView != nil) {
     	[alertView show];
-    }
+    }*/
 }
 
 - (void)didReceiveMemoryWarning
